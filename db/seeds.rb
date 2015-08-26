503de03f8d61ea2ce56e40dad9ca56dcf672eab0
rajesh23-drg <rmanjunath@dresourcesgroup.com>
2015-08-26 18:25:07 +0530

    Seed Update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,27 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
+#Organisation
+["DRG", "Mindfire", "MSCI"].each do |org|
+    orga = Organization.create!(name: org, url: "http://#{org}.com")
+    ["one","two"].each do |sub_org|
+      orga.sub_organizations.create(domain_name: "#{sub_org}.#{org}.com")
+    end
+end
 
-
-#Seed Roles
+#Roles
 ["SuperAdmin", "Admin", "User", "Supervisor", "Transporter"].each do |role|
    Role.create! name: role
  end
 
-# Seed For SuperAdmin
+#SuperAdmin
 super_admins = []
 super_admins << User.create!(:email => 'rajesh.rajsrv@gmail.com', :password => 'super_admin123$', :password_confirmation => 'super_admin123$')
 super_admins << User.create!(:email => 'ghoshsabyasachi0@gmail.com', :password => 'super_admin123$', :password_confirmation => 'super_admin123$')
 
-# Seed For Associations has_many Roles
-
+#Associations has_many Roles
 s_role = Role.find_by_name("SuperAdmin")
+drg_org = Organization.find_by_name("DRG")
 super_admins.each do |s|
-  UserRole.create! user_id: s.id, role_id: s_role.id
+  UserRole.create! user_id: s.id, role_id: s_role.id, sub_organization_id: drg_org.id
 end
