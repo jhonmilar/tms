992c476bdaf81503378da224991c417d8d967787
rajesh23-drg <rmanjunath@dresourcesgroup.com>
2015-08-26 17:44:03 +0530

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+#Seed Roles
+["SuperAdmin", "Admin", "User", "Supervisor", "Transporter"].each do |role|
+   Role.create! name: role
+ end
+
+# Seed For SuperAdmin
+super_admins = []
+super_admins << User.create!(:email => 'rajesh.rajsrv@gmail.com', :password => 'super_admin123$', :password_confirmation => 'super_admin123$')
+super_admins << User.create!(:email => 'ghoshsabyasachi0@gmail.com', :password => 'super_admin123$', :password_confirmation => 'super_admin123$')
+
+# Seed For Associations has_many Roles
+
+s_role = Role.find_by_name("SuperAdmin")
+super_admins.each do |s|
+  UserRole.create! user_id: s.id, role_id: s_role.id
+end
