d1cbe013c987cd6f098597863b0c0e9a3f7e3c33
Sabyasachi Ghosh <saghosh@dresourcesgroup.com>
2015-08-28 15:57:03 +0530

    did some refactor

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,7 +1,6 @@
 class Route < ActiveRecord::Base
 
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
-  validates_uniqueness_of :name, :scope => :sub_organization_id
+  validates :name, presence: true, uniqueness: {scope: :sub_organization_id}
 
   belongs_to :sub_organization
   has_many :user_routes
