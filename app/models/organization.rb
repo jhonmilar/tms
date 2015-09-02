10d9c7030ce03c1253dda605246f686c71446be5
rajesh23-drg <rmanjunath@dresourcesgroup.com>
2015-09-02 17:59:46 +0530

    Worked On Organization Controller

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,7 @@
 class Organization < ActiveRecord::Base
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :url, presence: true, uniqueness: { case_sensitive: false }
-  has_many :sub_organizations
+  has_many :sub_organizations, dependent: :destroy
 
   after_create :create_sub_organization
 
